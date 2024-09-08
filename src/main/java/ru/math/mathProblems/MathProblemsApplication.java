08f22b2a1946a4d7748656d79440d9d540d97074
freddycooper <almosttanonymous@gmail.com>
2024-09-09 01:38:56 +0300

    initial

---
+++ src/main/java/ru/math/mathProblems/MathProblemsApplication.java
@@ -0,0 +1,13 @@
+package ru.math.mathProblems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathProblemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MathProblemsApplication.class, args);
+	}
+
+}
